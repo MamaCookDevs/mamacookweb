456400ba326665063aa94a344502a8eec3b314ac
mamacookdevs@gmail.com <mamacookdevs@gmail.com>
2022-10-20 15:04:30 +0100

    Dev Commit

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,14 +3,15 @@ import{getApp, getApps, initializeApp } from 'firebase/app'
 import{getFirestore} from 'firebase/firestore'
 import{getStorage} from 'firebase/storage'
 const firebaseConfig = {
-    apiKey: "AIzaSyBvFrCkeHoX3s0zY9R4tma11dIWL0mQMe0",
-    authDomain: "prototype-b8a28.firebaseapp.com",
-    databaseURL: "https://prototype-b8a28-default-rtdb.firebaseio.com",
-    projectId: "prototype-b8a28",
-    storageBucket: "prototype-b8a28.appspot.com",
-    messagingSenderId: "789085467824",
-    appId: "1:789085467824:web:4d67a3179d782c880d3775"
-  };
+  apiKey: "AIzaSyDpJu1QK6ZtK3g6UJxo0lqvNCTTm4oPlzQ",
+  authDomain: "mama-cook-c8fe0.firebaseapp.com",
+  databaseURL: "https://mama-cook-c8fe0-default-rtdb.firebaseio.com",
+  projectId: "mama-cook-c8fe0",
+  storageBucket: "mama-cook-c8fe0.appspot.com",
+  messagingSenderId: "1040658349114",
+  appId: "1:1040658349114:web:fec5d87e3ed9f5b9b18903",
+  measurementId: "G-SP0DKEB384"
+};
   
   const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
 
