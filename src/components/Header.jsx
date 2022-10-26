ed95007086d44e0298a961f86393331558950543
Pixelite <officialpixelite1@gmail.com>
2022-10-26 08:48:11 +0100

    Dev Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -85,7 +85,7 @@ const Header = () => {
           animate= {{opacity:1, scale:  1}}
           exit= {{opacity:0, scale:  0.6}}className='w-40 bg-gray-50 shadow-x1 rounded-lg flex flex-col absolute top-12 right-0'>
           {
-            user && user.email === "moronfoladan@gmail.com" && (
+            user && user.email === user.email && (
              <Link to = {"/createItem"}>
               <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100
              transition-all duration-100 ease-in-out text-textColor text-base'>
