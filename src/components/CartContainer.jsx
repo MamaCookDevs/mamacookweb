37c80fced3bfb5a6cfbf108937ed782d9f351f3d
Pixelite <officialpixelite1@gmail.com>
2022-11-09 18:21:16 +0100

    Dev Message

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useStateValue } from "../context/StateProvider";
 
 const CartContainer = () => {
     const [{ cartShow, cartItems, user}, dispatch] = useStateValue()
