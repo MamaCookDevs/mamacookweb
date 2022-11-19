d83f3aa9c8056f21827cc50a42acdc7dc2218df1
Pixelite <officialpixelite1@gmail.com>
2022-11-19 12:06:36 +0100

    Dev Commit

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -65,7 +65,7 @@ const MainContainer = () => {
 
       <FoodContainer />
       <DrinkContainer />
-      <StripeContainer/>
+      
       <Footer />
       {cartShow && (
         <CartContainer />
