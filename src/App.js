4dc68c287ae21d11afb6b9e65ed0a5e25da93161
Pixelite <officialpixelite1@gmail.com>
2022-12-07 12:03:27 +0100

    Dev Commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import {
   CreateContainer,
   Header,
   MainContainer,
-  CheckoutForm,
-  StripeContainer
+  StripeContainer,
 } from "./components";
 import { useStateValue } from "./context/StateProvider";
 import { getAllFoodItems } from "./utils/firebaseFunctions";
 import { getAllDrinkItems } from "./utils/firebaseFunctions";
 import { actionType } from "./context/reducer";
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
 import Services from "./components/Services";
 
-import Mama from './../src/img/MamaLogo.png'
-
-const stripePromise = loadStripe(
-  "pk_test_51M1wtOK5OOmq9dlGCM4kLJY0gcy7IBMguQzfhxQhrlG0C0DsfkIOvCmqM3MtL7xx0FWiku81RQ69buR3foIAWIL400WFW6E5Ps"
-);
-
 const App = () => {
-  const options = {
-    // passing the client secret obtained from the server
-    clientSecret:
-      "sk_test_51M1wtOK5OOmq9dlGEyFkDa635JQtqBV7C6BB1YzuVhNNMlDkKsZeCZoAiHMiLcWRIjSdha6hntEWUzVHWeY32NrI00fNCTpXbM",
-  };
-
-  const [{ foodItems, drinkItems }, dispatch] = useStateValue();
-
-  const fetchData = async () => {
-    await getAllFoodItems().then((data) => {
-      dispatch({
-        type: actionType.SET_FOOD_ITEMS,
-        foodItems: data,
-      });
-    });
-    await getAllDrinkItems().then((data) => {
-      dispatch({
-        type: actionType.SET_DRINK_ITEMS,
-        drinkItems: data,
-      });
-    });
-  };
+  const [/*{ foodItems, drinkItems },*/ dispatch] = useStateValue();
 
   useEffect(() => {
-    fetchData();
+    const fetchData = async () => {
+      await getAllFoodItems().then((data) => {
+        dispatch({
+          type: actionType.SET_FOOD_ITEMS,
+          foodItems: data,
+        });
+      });
+      await getAllDrinkItems().then((data) => {
+        dispatch({
+          type: actionType.SET_DRINK_ITEMS,
+          drinkItems: data,
+        });
+      });
+    };
+
+    fetchData()
   }, []);
 
-  const [showItem, setShowItem] = useState(false);
+  //  const [showItem, setShowItem] = useState(false);
 
   return (
     <AnimatePresence exitBeforeEnter>
@@ -66,7 +51,6 @@ const App = () => {
             <Route path="/payment" element={<StripeContainer />} />
           </Routes>
         </main>
-        
       </div>
     </AnimatePresence>
   );
